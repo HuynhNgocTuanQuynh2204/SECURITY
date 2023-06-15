aad5daede0efb688ccba3aee8570ac19fcadade2
HuynhNgocTuanQuynh2204 <tuanquynh3452@gmail.com>
2023-06-16 03:31:21 +0700

    first name

--- Script/login.js
+++ Script/login.js
@@ -9,3 +9,14 @@ $(document).ready(function() {
         }
     });
 });
+$(document).ready(function() {
+    $('#eye1').click(function() {
+        $(this).toggleClass('open');
+        $(this).children('i').toggleClass('fa-eye-slash fa-eye');
+        if ($(this).hasClass('open')) {
+            $(this).prev().attr('type', 'text');
+        } else {
+            $(this).prev().attr('type', 'password');
+        }
+    });
+});
